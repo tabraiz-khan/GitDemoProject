0690ac27e70732e2ba75f96c092b689f858645ad
tabraiz-khan <tabraiz1816@gmail.com>
2026-05-12 19:42:09 +0530

    commited

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ print("Hi there we are starting with git")
 
 print("person 1 changed the project")
 
-print("person 2 changed the project")
+print("Person 2 is no more")
 
 print("also change in main")
